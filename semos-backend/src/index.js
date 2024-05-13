b833bb7d6144aa8325182ca0f398c05e69bdb472
Vangel22 <h.vangel22@gmail.com>
2024-05-13 18:42:05 +0200

    retry

--- semos-backend/src/index.js
+++ semos-backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json("Hello");
+  res.send("Express on Vercel");
 });
 
-app.listen(10000, (err) => {
-  if (err) throw Error(err);
-  console.log("Server started on port 10000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
