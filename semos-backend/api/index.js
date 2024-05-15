0b46e95cf1adbdbc59f783755ab35adb1e96a0a8
Vangel22 <h.vangel22@gmail.com>
2024-05-15 18:29:48 +0200

    Save

--- semos-backend/api/index.js
+++ semos-backend/api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send(`This is an environment variable: ${process.env.TEST}`);
 });
 
 app.get("/test", (req, res) => {
